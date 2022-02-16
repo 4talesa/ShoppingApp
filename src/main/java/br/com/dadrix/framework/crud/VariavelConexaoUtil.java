fb0c6693f0bf870dd0e421d86d4d8f075bbb3961
Rondinelli Fortalesa <rondinellifortalesa@gmail.com>
2022-02-16 08:56:24 -0300

    Utils para implementação do crud com hibernate

---
+++ src/main/java/br/com/dadrix/framework/crud/VariavelConexaoUtil.java
@@ -0,0 +1,13 @@
+package br.com.dadrix.framework.crud;
+
+/**
+ * Nome do Caminho do JNDI do datasource TomCat
+ * @author rondi
+ *
+ */
+
+public class VariavelConexaoUtil {
+	
+	private static String JAVA_COMP_ENV_JDBC_DATA_SOURCE = "java:/com/env/jdbc/datasource";
+
+}
